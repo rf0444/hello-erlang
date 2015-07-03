b275a6281b3bb5a7688ddfe480704f5ba97f9b47
r_fujisawa <r_fujisawa@denkiyagi.jp>
2015-07-03 12:14:41 +0900

    use start/stop

--- src/hello.erl
+++ src/hello.erl
@@ -1,17 +1,37 @@
 -module(hello).
 
+-export([start/0,
+         stop/0]).
 -export([auth_on_register/5,
          auth_on_publish/6,
          auth_on_subscribe/3]).
 
+-behaviour(auth_on_publish_hook).
+-behaviour(auth_on_register_hook).
+-behaviour(auth_on_subscribe_hook).
+
 -define(TABLE, ?MODULE).
+
+start() ->
+    error_logger:info_msg("hello start", []),
+    {ok, _} = application:ensure_all_started(hello),
+    case ets:info(?TABLE, name) of
+        ?TABLE ->
+            ok;
+        _ ->
+            ets:new(?TABLE, [public, named_table, {read_concurrency, true}]),
+            ok
+    end.
+
+stop() ->
+    error_logger:info_msg("hello stop", []),
+    application:stop(hello).
 
 auth_on_register(Peer, {_, ClientId}, UserName, Password, CleanSession) ->
     error_logger:info_msg("auth_on_register - peer: ~p, client id: ~p, user: ~p, pass: ~p, clean: ~p", [Peer, ClientId, UserName, Password, CleanSession]),
-    Table = table(),
     case {UserName, Password} of
         {X, X} ->
-            ets:insert(Table, {ClientId, UserName}),
+            ets:insert(?TABLE, {ClientId, UserName}),
             error_logger:info_msg("registered client ~p by user ~p", [ClientId, UserName]),
             ok;
         _ -> {error, invalid_credentials}
@@ -19,8 +39,7 @@ auth_on_register(Peer, {_, ClientId}, UserName, Password, CleanSession) ->
 
 auth_on_publish(_, {_, ClientId}, QoS, Topic, Payload, IsRetain) ->
     error_logger:info_msg("auth_on_publish - client id: ~p, qos: ~p, topic: ~p, retain: ~p, payload: ~p", [ClientId, QoS, Topic, IsRetain, Payload]),
-    Table = table(),
-    case ets:lookup(Table, ClientId) of
+    case ets:lookup(?TABLE, ClientId) of
         [{_, "guest"}] ->
             error_logger:info_msg("guest user cannot publish", []),
             {error, whatever};
@@ -34,8 +53,7 @@ auth_on_publish(_, {_, ClientId}, QoS, Topic, Payload, IsRetain) ->
 
 auth_on_subscribe(_, {_, ClientId}, Topics) ->
     error_logger:info_msg("auth_on_subscribe - user id: ~p, topic: ~p", [ClientId, Topics]),
-    Table = table(),
-    case ets:lookup(Table, ClientId) of
+    case ets:lookup(?TABLE, ClientId) of
         [{_, "nosub"}] ->
             error_logger:info_msg("nosub user cannot subscribe", []),
             {error, whatever};
@@ -45,13 +63,4 @@ auth_on_subscribe(_, {_, ClientId}, Topics) ->
         _ ->
             error_logger:info_msg("invalid subscribe user id: ~p", [ClientId]),
             {error, whatever}
-    end.
-
-table() ->
-    case ets:info(?TABLE, name) of
-        ?TABLE ->
-            ?TABLE;
-        _ ->
-            ets:new(?TABLE, [public, named_table, {read_concurrency, true}]),
-            ?TABLE
     end.
